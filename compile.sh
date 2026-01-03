40041f103e0eac81b29961e9c36684fdb5ff7563
Riaru Moda <riarumoda@gmail.com>
2026-01-03 10:55:53 +0800

    import: import files from sm6150 repository

---
+++ compile.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+##################################################
+# Unofficial OneUI Perf kernel Compile Script
+# Based on the original compile script by vbajs
+# Forked by Riaru Moda
+##################################################
+
+# Help message
+help_message() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Options:"
+  echo "  --ksu          Enable KernelSU support"
+  echo "  --no-ksu       Disable KernelSU support"
+  echo "  --help         Show this help message"
+}
+
+# Environment setup
+setup_environment() {
+  echo "Setting up build environment..."
+  export ARCH=arm64
+  export SUBARCH=arm64
+  export HEADER_ARCH=arm64
+  export KBUILD_BUILD_USER=riaru
+  export KBUILD_BUILD_HOST=ximiedits
+  export GCC64_DIR=$PWD/gcc64
+  export KSU_SETUP_URI="https://github.com/KernelSU-Next/KernelSU-Next"
+  export KSU_BRANCH="legacy"
+}
+
+# Toolchain setup
+setup_toolchain() {
+  echo "Setting up toolchains..."
+
+  if [ ! -d "$PWD/gcc64" ]; then
+    echo "Downloading GCC..."
+    git clone git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 gcc64
+  else
+    echo "Local gcc dirs found, using them."
+  fi
+}
+
+# Update PATH
+update_path() {
+  echo "Updating PATH..."
+  export PATH="$GCC64_DIR/bin/:/usr/bin:$PATH"
+}
+
+# KSU Setup
+setup_ksu() {
+  local arg="$1"
+  if [[ "$arg" == "--ksu" ]]; then
+    echo "Setting up KernelSU..."
+    echo "CONFIG_KSU=y" >> arch/arm64/configs/a9y18qlte_eur_open_defconfig
+    echo "CONFIG_KSU_LSM_SECURITY_HOOKS=y" >> arch/arm64/configs/a9y18qlte_eur_open_defconfig
+    echo "CONFIG_KSU_MANUAL_HOOKS=y" >> arch/arm64/configs/a9y18qlte_eur_open_defconfig
+    patch -p1 < ksumakefile.patch
+    git clone "$KSU_SETUP_URI" -b "$KSU_BRANCH" KernelSU
+    cd drivers
+    ln -sfv ../KernelSU/kernel kernelsu
+    cd ..
+  elif [[ "$arg" == "--no-ksu" ]]; then
+    echo "KernelSU setup skipped."
+  fi
+}
+
+# Compile kernel
+compile_kernel() {
+  echo -e "\nStarting compilation..."
+  sed -i 's/# CONFIG_LOCALVERSION is not set/CONFIG_LOCALVERSION="-perf-neon"/' arch/arm64/configs/a9y18qlte_eur_open_defconfig
+  sed -i 's/CONFIG_LOCALVERSION_AUTO=y/# CONFIG_LOCALVERSION_AUTO is not set/' arch/arm64/configs/a9y18qlte_eur_open_defconfig
+  make O=out ARCH=arm64 a9y18qlte_eur_open_defconfig
+  make -j$(nproc --all) \
+    ARCH=arm64 \
+    O=out \
+    SUBARCH=arm64 \
+    CC=aarch64-linux-android-gcc \
+    LD=aarch64-linux-android-ld.bfd \
+    AR=aarch64-linux-android-ar \
+    AS=aarch64-linux-android-as \
+    NM=aarch64-linux-android-nm \
+    OBJCOPY=aarch64-linux-android-objcopy \
+    OBJDUMP=aarch64-linux-android-objdump \
+    STRIP=aarch64-linux-android-strip \
+    CROSS_COMPILE=aarch64-linux-android-
+}
+
+# Main function
+main() {
+  case "$1" in
+    --help)
+      help_message
+      exit 0
+      ;;
+  esac
+  setup_environment
+  setup_toolchain
+  update_path
+  setup_ksu "$1"
+  compile_kernel
+}
+
+# Run the main function
+main "$1"
